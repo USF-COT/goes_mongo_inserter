d6d9112afa8aa9ff43db2285fe75cfa01b05b6db
Michael Lindemuth <mlindemu@newcomps>
2013-09-18 17:44:58 +0000

    Now accept multiple connection strings for COMPS database

--- goes_mongo_inserter/lib/trdi_adcp_psql_inserter.py
+++ goes_mongo_inserter/lib/trdi_adcp_psql_inserter.py
@@ -14,25 +14,26 @@ log = logging.getLogger("GMI")
 from itertools import izip
 
 
-def insert_to_pgsql(pd0_data, config):
-    host = os.environ.get('COMPSDB_HOST', 'localhost')
-    port = os.environ.get('COMPSDB_PORT', '5432')
-    dbname = os.environ.get('COMPSDB_DATABASE', 'comps')
-    user = os.environ.get('COMPSDB_USER', 'postgres')
-    password = os.environ.get('COMPSDB_PASS', '')
+class Old_COMPS_DB(object):
+    def __init__(self, connection_string):
+        self.conn = psycopg2.connect(connection_string)
+        self.cur = self.conn.cursor()
 
+
+def insert_to_pgsql(pd0_data, config):
     table = config['pg_table']
 
-    conn_string = (
-        'host=%s dbname=%s port=%s '
-        'user=%s password=%s' % (host, dbname, port, user, password)
-    )
+    conn_strings_env = os.environ.get('OLD_DBS_CONNECTION_STRINGS')
+    conn_strings = conn_strings_env.split(',')
 
-    log.info(conn_string)
-
-    conn = psycopg2.connect(conn_string)
-
-    cur = conn.cursor()
+    connections = []
+    for conn_string in conn_strings:
+        try:
+            conn = Old_COMPS_DB(conn_string)
+            connections.append(conn)
+        except Exception, e:
+            log.error('Error connecting to database with parameters %s. '
+                      'Message: %s' % (conn_string, e))
 
     timestamp = pd0_data['timestamp']
     bin_1_distance = pd0_data['fixed_leader']['bin_1_distance']
@@ -49,15 +50,21 @@ def insert_to_pgsql(pd0_data, config):
         speed = round(dataset[1], 2)
         direction = round(dataset[2], 2)
         depth = float(bin_1_distance) + float(depth_cell_length) * (i-1)
-        cur.execute('INSERT INTO %s '
-                    '(timestamp, bin_number, '
-                    'eastward_current, northward_current,'
-                    'current_speed, current_direction, water_depth) VALUES '
-                    '(\'%s\', %s, %s, %s, %s, %s, %s)'
-                    % (table, timestamp, i, eastward, northward,
-                       speed, direction, depth)
-                    )
+        for conn in connections:
+            try:
+                conn.cur.execute('INSERT INTO %s '
+                                 '(timestamp, bin_number, '
+                                 'eastward_current, northward_current,'
+                                 'current_speed, current_direction, water_depth) '  # NOQA
+                                 'VALUES (\'%s\', %s, %s, %s, %s, %s, %s)'
+                                 % (table, timestamp, i, eastward, northward,
+                                    speed, direction, depth)
+                                 )
+            except Exception, e:
+                log.error('Error inserting ADCP data '
+                          'into old COMPS database. Error: %s' % e)
 
-    conn.commit()
-    cur.close()
-    conn.close()
+    for connection in connections:
+        connection.conn.commit()
+        connection.cur.close()
+        connection.conn.close()
